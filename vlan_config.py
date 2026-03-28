743d909f3a0b86b5911ef101e177de4d2cc30c87
Ahndre T. R. Walters <waltersahndre@gmail.com>
2026-03-28 01:56:26 -0400

    Update

--- vlan_config.py
+++
@@ -1,3 +0,0 @@
-vlans = {10: "Ministers_Office", 20: "Finance", 30: "IT", 40: "Public_Relations", 50: "Internal_Services", 60: "Guest_WiFi", 99: "Servers"}
-for id, name in vlans.items():
-    print(f"vlan {id}\n name {name}")
